8f20002cf1ae839c6b21c4b8eb5eb916e79e0f28
Loudi <loudimail@gmail.com>
2024-05-13 13:09:27 +0200

    hopefully the bom input, post drawing and beam drawing is completely finished, needs to get tested with an actual drawing

--- src/main/java/app/web/services/SvgCarport.java
+++ src/main/java/app/web/services/SvgCarport.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Locale;
 
 public class SvgCarport {
-    private static List<Plank> testOrder = new ArrayList<>();
     private static int id = 0;
     private UnitConversion unitConversion = new UnitConversion(600,780);
     private Svg carportSvg = new Svg(0,0,"0 0 900 700", "100%","auto");
@@ -20,35 +19,18 @@ public class SvgCarport {
 //    private List<Plank> rafters = plankOrganiser(getTestOrder(),Plank.RAFTER);
 //    private List<Plank> beams = plankOrganiser(getTestOrder(),Plank.BEAM);
 //    private List<Plank> posts = plankOrganiser(getTestOrder(),Plank.POST);
-    private List<Plank> rafters = bom.getRafters().values().stream().toList();
-    private List<Plank> beams = bom.getBeams().values().stream().toList();
-    private List<Plank> posts = bom.getPosts().values().stream().toList();
+    private List<Plank> rafters;
+    private List<Plank> beams;
+    private List<Plank> posts;
     private double beamToOtherMath;
-    private double beamToPostLength;
 
     public SvgCarport(Bom bom) {
         this.bom = bom;
+        rafters = bom.getRafters().values().stream().toList();
+        beams = bom.getBeams().values().stream().toList();
+        posts = bom.getPosts().values().stream().toList();
     }
 
-    public static List<Plank> getTestOrder() {
-        Plank beam = new Plank(id++,25,200,4200,Plank.BEAM,0);
-        beam.setAmount(4);
-        testOrder.add(beam);
-
-        Plank rafter = new Plank(id++,45,195,6000,Plank.RAFTER,0);
-        rafter.setAmount(16);
-        testOrder.add(rafter);
-
-        Plank post = new Plank(id++,97,97,3000,Plank.POST,0);
-        post.setAmount(6);
-        testOrder.add(post);
-
-//        Plank board = new Plank(id++,45,195,6000,Plank.BOARD,0);
-//        board.setAmount(15);
-//        testOrder.add(board);
-
-        return testOrder;
-    }
 
     public String drawCarport(){
         Locale.setDefault(new Locale("US"));
@@ -67,7 +49,6 @@ public class SvgCarport {
         beamDrawer();
         postDrawer();
         rafterInnerDrawer();
-//        boardDrawer();
 
         return carportSvg.toString();
     }
@@ -75,12 +56,10 @@ public class SvgCarport {
     public void rafterDrawer(){
         carportSvg.addRectangle(0, 0, rafters.get(0).getDrawHeight(unitConversion), rafters.get(0).getDrawWidth(unitConversion), rectStandardStyle);
         carportSvg.addRectangle(800 - (rafters.get(0).getDrawWidth(unitConversion)), 0, rafters.get(0).getDrawHeight(unitConversion), rafters.get(0).getDrawWidth(unitConversion), rectStandardStyle);
-        //made minus total rafter amount by 2?
         rafters.get(0).setAmount(rafters.get(0).getAmount()-2);
     }
 
     public void rafterInnerDrawer(){
-        double minDistance = 0; // not used yet
         double distanceEach = (beamToOtherMath / (rafters.get(0).getAmount() + 1) + rafters.get(0).getDrawWidth(unitConversion) / (rafters.get(0).getAmount() + 1) );
 
         for (int i = 1; i < rafters.get(0).getAmount()+1; i++) {
@@ -90,17 +69,18 @@ public class SvgCarport {
 
     public void beamDrawer(){
         double boardDrawingFillLength = (unitConversion.DRAW_WIDTH - (rafters.get(0).getDrawWidth(unitConversion) * 2));
-        double totalDrawLength = beams.get(0).getAmount() * beams.get(0).getDrawWidth(unitConversion);
-//        double totalDrawLength;
+        double totalDrawLength = 0;
 
+        for (Plank beam : beams) {
+            for (int i = 0; i < beam.getAmount(); i++) {
+                totalDrawLength = totalDrawLength + beam.getDrawWidth(unitConversion);
+            }
+        }
 
-
-        System.out.println(beams.get(0).getAmount());
-        System.out.println(beams.get(0).getLength());
         System.out.println("usable length in total: "+totalDrawLength);
         System.out.println("usable length for each side: "+totalDrawLength/2);
         System.out.println("length needing to be filled for each board side: "+(unitConversion.DRAW_WIDTH - (rafters.get(0).getDrawWidth(unitConversion) * 2)));
-        System.out.println("extra length: "+((totalDrawLength / 2) - boardDrawingFillLength));
+        System.out.println("extra length per side: "+((totalDrawLength / 2) - boardDrawingFillLength));
 
         if ((totalDrawLength / 2) >= (unitConversion.DRAW_WIDTH - (rafters.get(0).getDrawWidth(unitConversion) * 2))){
         carportSvg.addRectangle(rafters.get(0).getDrawWidth(unitConversion),50,beams.get(0).getDrawHeight(unitConversion),boardDrawingFillLength,rectStandardStyle);
@@ -108,85 +88,36 @@ public class SvgCarport {
         }
 
         beamToOtherMath = boardDrawingFillLength;
-        beamToPostLength = beams.get(0).getDrawWidth(unitConversion);
     }
 
     public void postDrawer(){
-        double postCheckerThing = beamToOtherMath / beamToPostLength;
 
         carportSvg.addRectangle(50 + rafters.get(0).getDrawWidth(unitConversion),50 - (1.5 * beams.get(0).getDrawHeight(unitConversion)), posts.get(0).getDrawHeight(unitConversion),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
         carportSvg.addRectangle(750 - rafters.get(0).getDrawWidth(unitConversion),50 - (1.5 * beams.get(0).getDrawHeight(unitConversion)), posts.get(0).getDrawHeight(unitConversion),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
         carportSvg.addRectangle(50 + rafters.get(0).getDrawWidth(unitConversion),550 - (1.5 * beams.get(0).getDrawHeight(unitConversion)), posts.get(0).getDrawHeight(unitConversion),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
         carportSvg.addRectangle(750 - rafters.get(0).getDrawWidth(unitConversion),550 - (1.5 * beams.get(0).getDrawHeight(unitConversion)), posts.get(0).getDrawHeight(unitConversion),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
+        posts.get(0).setAmount(posts.get(0).getAmount()-4);
 
-        System.out.println("posts for each side (aside from 2 corner ones): "+(int) postCheckerThing);
-        for (int i = 0; i < (int) postCheckerThing; i++) {
-        carportSvg.addRectangle(beamToPostLength + rafters.get(0).getDrawWidth(unitConversion),50 - (1.5 * (unitConversion.heightMmToDrawUnits(beams.get(0).getHeight()))), unitConversion.heightMmToDrawUnits(posts.get(0).getHeight()),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
-        carportSvg.addRectangle(beamToPostLength + rafters.get(0).getDrawWidth(unitConversion),550 - (1.5 * (unitConversion.heightMmToDrawUnits(beams.get(0).getHeight()))), unitConversion.heightMmToDrawUnits(posts.get(0).getHeight()),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
+        //TODO: check if it places them correctly on an actual drawing
+        for (Plank beam : beams) {
+            for (int i = 1; i < beam.getAmount()+1; i++) {
+                    if (0 < beamToOtherMath - (unitConversion.widthMmToDrawUnits(beam.getLength()) * i) + rafters.get(0).getDrawWidth(unitConversion) && 1 < posts.get(0).getAmount()){
+
+                    carportSvg.addRectangle((unitConversion.widthMmToDrawUnits(beam.getLength()) * i) + rafters.get(0).getDrawWidth(unitConversion),50 - (1.5 * (unitConversion.heightMmToDrawUnits(beams.get(0).getHeight()))), unitConversion.heightMmToDrawUnits(posts.get(0).getHeight()),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
+                    carportSvg.addRectangle((unitConversion.widthMmToDrawUnits(beam.getLength()) * i) + rafters.get(0).getDrawWidth(unitConversion),550 - (1.5 * (unitConversion.heightMmToDrawUnits(beams.get(0).getHeight()))), unitConversion.heightMmToDrawUnits(posts.get(0).getHeight()),posts.get(0).getDrawWidth(unitConversion),rectStandardStyle);
+                        System.out.println("places posts at: "+(unitConversion.widthMmToDrawUnits(beam.getLength()) * i) + rafters.get(0).getDrawWidth(unitConversion));
+                    posts.get(0).setAmount(posts.get(0).getAmount()-2);
+                    }
+                    else {
+                        System.out.println("couldn't place posts at: "+(unitConversion.widthMmToDrawUnits(beam.getLength()) * i) + rafters.get(0).getDrawWidth(unitConversion));
+                    }
+                System.out.println("amount of posts left: "+posts.get(0).getAmount());
+                    if (posts.get(0).getAmount() == 0){
+                        break;
+                    }
+            }
         }
+
     }
 
-    public void boardDrawer(){
-//        double minDistance = 0;
-//
-//        for (int i = 1; i < boards.get(0).getAmount()+1; i++) {
-//            if (i == 1){
-//        carportSvg.addRectangle(minDistance + rafters.get(0).getDrawWidth(unitConversion), 0,boards.get(0).getDrawHeight(unitConversion),boards.get(0).getDrawWidth(unitConversion),rectStandardStyle);
-//            }
-//            if (i > 1 && 1 < beamToOtherMath - ((minDistance * i) + rafters.get(0).getDrawWidth(unitConversion) + boards.get(0).getDrawWidth(unitConversion))){
-//        carportSvg.addRectangle((minDistance * i) + rafters.get(0).getDrawWidth(unitConversion) + boards.get(0).getDrawWidth(unitConversion), 0,boards.get(0).getDrawHeight(unitConversion),boards.get(0).getDrawWidth(unitConversion),rectStandardStyle);
-////                System.out.println(beamToOtherMath - ((minDistance * i) + rafters.get(0).getDrawWidth(unitConversion) + boards.get(0).getDrawWidth(unitConversion)));
-////                System.out.println(((minDistance * i) + rafters.get(0).getDrawWidth(unitConversion) + boards.get(0).getDrawWidth(unitConversion)));
-////                System.out.println(beamToOtherMath);
-//            } else {
-//                if (i > 1){
-//                System.out.println("number board of not used: " + i);
-//                }
-//            }
-//        }
-    }
-
-    public ArrayList<Plank> plankOrganiser(List<Plank> planks, int type){
-        ArrayList<Plank> plankArrayList = new ArrayList<>();
-        switch (type) {
-            case 0: //board
-                for (Plank plank : planks) {
-                    if (plank.getType().equals(0)){
-                        plankArrayList.add(plank);
-                    }
-                }
-                break;
-            case 1: //lath
-                for (Plank plank : planks) {
-                    if (plank.getType().equals(1)){
-                        plankArrayList.add(plank);
-                    }
-                }
-                break;
-            case 2: //beam
-                for (Plank plank : planks) {
-                    if (plank.getType().equals(2)){
-                        plankArrayList.add(plank);
-                    }
-                }
-                break;
-            case 3: //rafter
-                for (Plank plank : planks) {
-                    if (plank.getType().equals(3)){
-                        plankArrayList.add(plank);
-                    }
-                }
-                break;
-            case 4: //post
-                for (Plank plank : planks) {
-                    if (plank.getType().equals(4)){
-                        plankArrayList.add(plank);
-                    }
-                }
-                break;
-            default:
-                //TODO: throw an error or something
-        }
-        return plankArrayList;
-    }
 }
