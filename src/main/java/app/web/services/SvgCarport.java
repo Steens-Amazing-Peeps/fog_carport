a2105b9b13325ffcbf045750f1d7606624a9e040
Loudi <loudimail@gmail.com>
2024-05-16 15:54:11 +0200

    drawCarport setup needs named varibles for the numbers used

--- src/main/java/app/web/services/SvgCarport.java
+++ src/main/java/app/web/services/SvgCarport.java
@@ -13,7 +13,7 @@ import java.util.Locale;
 public class SvgCarport {
     private static int id = 0;
     private UnitConversion unitConversion;
-    private Svg carportSvg = new Svg(0,0,"0 0 900 700", "100%","auto");
+    private Svg carportSvg = new Svg(this.origoXYValue,this.origoXYValue,"0 0 900 700", "100%","auto"); //the viewbox is a made to match the html page's div
 
     private final String rectStandardStyle = "stroke: #000000; stroke-width: 1px; fill: #ffffff";
 
@@ -23,6 +23,7 @@ public class SvgCarport {
     private List<Plank> posts;
     private double totalBeamFillingLength;
     private double drawWidth = unitConversion.DRAW_WIDTH.intValue();
+    private final int origoXYValue = 0;
 
     private final int offsetFromRight = 750;
     private final int offsetFromLeft = 50;
@@ -50,9 +51,9 @@ public class SvgCarport {
 
 
     public String drawCarport(){ //TODO: make constants for all manually placed numbers
-        Locale.setDefault(new Locale("US"));
+        Locale.setDefault(new Locale("US")); //needed if you want to use decimals for svg stuff
 //        outer svg setup
-        carportSvg.addRectangle(0,0,700,900,"stroke: #000000; stroke-width: 1px; fill: none");
+        carportSvg.addRectangle(origoXYValue,origoXYValue,700,900,"stroke: #000000; stroke-width: 1px; fill: none");
         carportSvg.addArrow(50, 620,50,20);
         carportSvg.addArrow(80, 650,880,650);
         carportSvg.addText(30,300,-90, MetricConversion.mmToCm(unitConversion.getCarportHeight())+" cm");
@@ -60,7 +61,7 @@ public class SvgCarport {
 
 //        inner svg setup
         carportSvg.addSvg(80,20,"0 0 800 600", "800", "600");
-        carportSvg.addRectangle(0,0,600,800,"stroke: #000000; stroke-width: 1px; fill: #ffffff");
+        carportSvg.addRectangle(origoXYValue,origoXYValue,600,800,"stroke: #000000; stroke-width: 1px; fill: #ffffff");
 //        methods for drawing the actual carport
         rafterDrawer();
         beamDrawer();
@@ -74,23 +75,26 @@ public class SvgCarport {
         rafterWidth = rafters.get(0).getDrawWidth(unitConversion);
         rafterHeight = rafters.get(0).getDrawHeight(unitConversion);
         rafterAmount = rafters.get(0).getAmount();
-        carportSvg.addRectangle(0, 0, rafterHeight, rafterWidth, rectStandardStyle);
-        carportSvg.addRectangle(drawWidth - rafterWidth, 0, rafterHeight, rafterWidth, rectStandardStyle);
-        rafterAmount = rafterAmount - 2;
+        int amountIfFixedSideRafters = 2;
+
+        carportSvg.addRectangle(origoXYValue, origoXYValue, rafterHeight, rafterWidth, rectStandardStyle);
+        carportSvg.addRectangle(drawWidth - rafterWidth, origoXYValue, rafterHeight, rafterWidth, rectStandardStyle);
+        rafterAmount = rafterAmount - amountIfFixedSideRafters;
     }
 
     public void rafterInnerDrawer(){
-        double distanceEach = (totalBeamFillingLength / (rafterAmount + 1) + rafterWidth / rafterAmount );
+        int mathEqualSpacingDifferenceConstant = 1;
+        double distanceEach = (totalBeamFillingLength / (rafterAmount + mathEqualSpacingDifferenceConstant) + rafterWidth / rafterAmount );
 
-        for (int i = 1; i < rafterAmount+1; i++) {
-            carportSvg.addRectangle(xCenterFigureNegative((distanceEach * i),rafterWidth), 0,rafterHeight,rafterWidth,rectStandardStyle);
+        for (int i = 0; i < rafterAmount; i++) {
+            carportSvg.addRectangle(xCenterFigureNegative((distanceEach * (i+1)),rafterWidth), origoXYValue,rafterHeight,rafterWidth,rectStandardStyle);
         }
     }
 
     public void beamDrawer(){
         beamHeight = beams.get(0).getDrawHeight(unitConversion);
-        
-        totalBeamFillingLength = drawWidth - (rafterWidth * 2);
+        int multiplyValueToGetBothRafterWidths = 2;
+        totalBeamFillingLength = drawWidth - (rafterWidth * multiplyValueToGetBothRafterWidths);
 
         carportSvg.addRectangle(rafterWidth,offsetFromTop,beamHeight,totalBeamFillingLength,rectStandardStyle);
         carportSvg.addRectangle(rafterWidth,offsetFromBottom,beamHeight,totalBeamFillingLength,rectStandardStyle);
@@ -101,18 +105,22 @@ public class SvgCarport {
         double postWidth = posts.get(0).getDrawWidth(unitConversion);
         double postHeight = posts.get(0).getDrawHeight(unitConversion);
         double postAmount = posts.get(0).getAmount();
+        int amountOfFixedCornerPosts = 4;
 
         carportSvg.addRectangle(offsetFromLeft + rafterWidth,yCenterFigure(offsetFromTop,beamHeight) , postHeight,postWidth,rectStandardStyle);
         carportSvg.addRectangle(offsetFromRight - rafterWidth,yCenterFigure(offsetFromTop,beamHeight), postHeight,postWidth,rectStandardStyle);
         carportSvg.addRectangle(offsetFromLeft + rafterWidth,yCenterFigure(offsetFromBottom,beamHeight), postHeight,postWidth,rectStandardStyle);
         carportSvg.addRectangle(offsetFromRight - rafterWidth,yCenterFigure(offsetFromBottom,beamHeight), postHeight,postWidth,rectStandardStyle);
-        postAmount = postAmount - 4;
+        postAmount = postAmount - amountOfFixedCornerPosts;
 
         double xValuePosts = 0;
         for (Plank beam : beams) {
             beamAmount = beam.getAmount();
             beamWidth = beam.getDrawWidth(unitConversion);
-            int foriLoopAmount = (int) (beamAmount / 2);
+            int beamAmountDivideToHalfNumber = 2;
+            int foriLoopAmount = (int) (beamAmount / beamAmountDivideToHalfNumber);
+            int amountOfPostsPlacedPerLoop = 2;
+
             for (int i = 0; i < foriLoopAmount; i++) {
                     if (0 < totalBeamFillingLength - (beamWidth * (i+1)) + rafterWidth && 1 < postAmount){
 
@@ -126,7 +134,7 @@ public class SvgCarport {
                             carportSvg.addRectangle(xValuePosts,yCenterFigure(offsetFromBottom,beamHeight), postHeight,postWidth,rectStandardStyle);
                         }
                         System.out.println("posts placed at: "+xValuePosts);
-                        postAmount = postAmount - 2;
+                        postAmount = postAmount - amountOfPostsPlacedPerLoop;
                     }
                     else {
                         System.out.println("couldn't place posts at: "+xValuePosts);
